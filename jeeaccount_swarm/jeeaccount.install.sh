7bb306d5f985cbc3bde489e68efa1aa7a440f5a1
hoaaq <auhoa1997@gmail.com>
2021-04-17 12:16:20 +0700

    hoaaq

--- jeeaccount_swarm/jeeaccount.install.sh
+++ jeeaccount_swarm/jeeaccount.install.sh
@@ -4,8 +4,7 @@ export DOMAIN=jee.vn
 export VAULT_ENDPOINT=http://vault_vault:8200
 export VAULT_TOKEN=s.j9o2MvtdSSeTT0dvFH8SAbQ9
 export KAFKA_BROKER=kafka1:9999,kafka2:9999,kafka3:9999
-export CONNECTION_STRING="Data Source=192.168.199.4,1433;Initial Catalog=JeeRequest;User ID=jeerequest;Password=Je3reQu3ts\$Dew"
-export PUBLIC_LINK_API=https://jeerequest-api.jee.vn
-export JEEACCOUNT_API=https://jeeaccount-api.jee.vn/
+export CONNECTION_STRING="Data Source=192.168.199.4,1433;Initial Catalog=JeeAccount;User ID=jeeaccount;Password=Je3@cCountD\$ev"
+export PUBLIC_LINK_API=https://jeeaccount-api.jee.vn
 
-docker stack deploy -c ./jeerequest.swarm.yml --with-registry-auth jeerequest
+docker stack deploy -c ./jeeaccount.swarm.yml --with-registry-auth jeeaccount
