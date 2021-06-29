08cc56e1511b3744707e058eb8c596e2f6d03ded
hoaaq <auhoa1997@gmail.com>
2021-06-29 09:35:44 +0700

    hoaaq

--- scripts/env/prod.sh
+++ scripts/env/prod.sh
@@ -10,7 +10,7 @@ export DOMAIN=jee.vn
 export VAULT_ENDPOINT=http://vault_vault:8200
 export VAULT_TOKEN=s.ucWXuLsJ4pRQkp7aCuL81L6R
 export KAFKA_BROKER=kafka1:9999,kafka2:9999,kafka3:9999
-export HOST_CDN=cdn.jee.com.vn
+export HOST_CDN=cdn.jee.vn
 export HOST_JEECOMMENT_API=https://jeecomment-api.jee.vn
 export APP_CONNECTION="Data Source=192.168.199.4,1433;Initial Catalog=JeeAccount;User ID=jeeaccount;Password=Je3@cCountD\$ev"
 export APP_KEY_JEECUSTOMER="sJoPSvHUHmbFvbzGC8Yp4LKUZtQ6M6pRalM3IST9cMJNGk2v2ZQETFJp87XNWAoQ4tUWGIRPwQRBpI1vtWSDgYfzbjXJZwNNT2PujJmP1YNznfURNRjLD10N0fx7qshYsDsk2dE49ifjJ4xnl0yOIUoOLwHf3dm9"
