79d3e194e68b961c12963e31b164a329b0e1f1f7
hoaaq <auhoa1997@gmail.com>
2021-06-29 19:09:08 +0700

    hoaaq

---
+++ BackEnd/JeeAccount/src/environments/environment.stage.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  appVersion: 'v717demo1',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: true,
+  apiUrl: 'api',
+  HOST_JEEACCOUNT_API: 'https://jeeaccount-api.jee.com.vn',
+  HOST_IDENTITYSERVER_API: 'https://identityserver.jee.com.vn',
+  HOST_PORTAL_API: 'https://portal.jee.com.vn',
+  HOST_JEECOMMENT_API: 'https://jeecomment-api.jee.com.vn',
+};
--- scripts/env/test.sh
+++ scripts/env/test.sh
@@ -5,12 +5,12 @@ export REGISTRY_PUBLISHER=dps
 export REGISTRY_USERNAME=admin
 export REGISTRY_PASSWORD=registrypw123
 export PROJECT_NAME=jeeaccount
-
 export DOMAIN=jee.com.vn
+
 export VAULT_ENDPOINT=http://vault_vault:8200
 export VAULT_TOKEN=s.Xz0cUwdkYd8zxgC7TF0L8miO
 export KAFKA_BROKER=kafka1:9999,kafka2:9999,kafka3:9999
 export HOST_CDN=cdn.jee.com.vn
 export HOST_JEECOMMENT_API=https://jeecomment-api.jee.com.vn
-export APP_CONNECTION="Data Source=192.168.199.4,1433;Initial Catalog=JeeAccount;User ID=jeeaccount;Password=Je3@cCountD\$ev"
+export APP_CONNECTION="Data Source=192.168.2.92,1433;Initial Catalog=JeeAccount;User ID=jeeaccount;Password=Je3@cCountD\$ev"
 export APP_KEY_JEECUSTOMER="sJoPSvHUHmbFvbzGC8Yp4LKUZtQ6M6pRalM3IST9cMJNGk2v2ZQETFJp87XNWAoQ4tUWGIRPwQRBpI1vtWSDgYfzbjXJZwNNT2PujJmP1YNznfURNRjLD10N0fx7qshYsDsk2dE49ifjJ4xnl0yOIUoOLwHf3dm9"
