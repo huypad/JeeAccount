9b47b8f1d200c14663f64b34d94138165397c09c
windsora1998 <windsora1998@gmail.com>
2021-08-16 10:52:58 +0700

    .

--- BackEnd/JeeAccount/src/app/pages/pages-routing.module.ts
+++ BackEnd/JeeAccount/src/app/pages/pages-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: 'info',
+        redirectTo: 'Management/AccountManagement',
         pathMatch: 'full',
       },
       {
