1550fd74750503e634795aa490f1572f332ca156
windsora1998 <41285055+windsora1998@users.noreply.github.com>
2022-01-13 11:11:51 +0700

    phong update

--- JeeAccount/Controllers/JeeHRController.cs
+++ JeeAccount/Controllers/JeeHRController.cs
@@ -1,6 +1,4 @@
-﻿using DPSinfra.Logger;
-using JeeAccount.Models.JeeHR;
-using Microsoft.Extensions.Logging;
+﻿using JeeAccount.Models.JeeHR;
 using Newtonsoft.Json;
 using RestSharp;
 using System;
@@ -14,10 +12,9 @@ namespace JeeAccount.Controllers
 {
     public class JeeHRController
     {
-        public JeeHRController(string HOST_API_JEEHR, ILogger<JeeHRController> logger)
+        public JeeHRController(string HOST_API_JEEHR)
         {
             _HOST_API_JEEHR = HOST_API_JEEHR;
-            _logger = logger;
         }
 
         private readonly string _HOST_API_JEEHR;
@@ -25,12 +22,10 @@ namespace JeeAccount.Controllers
         private const string GET_DSNHANVIEN_THEOQUANLYTRUCTIEP = "api/interaction/getDSNhanVienTheoQLTT";
         private const string GET_COCAUTOCHUC = "api/interaction/getCoCauToChuc";
         private const string GET_CHUCVUCHUCDANH = "api/interaction/getDSChucVuTheoChucDanh";
-        private readonly ILogger<JeeHRController> _logger;
 
         public async Task<ReturnJeeHR<NhanVienJeeHR>> GetDSNhanVien(string access_token)
         {
             string url = $"{_HOST_API_JEEHR}/{GET_DSNHANVIEN}";
-
 
             using (var client = new HttpClient())
             {
@@ -72,14 +67,6 @@ namespace JeeAccount.Controllers
 
         public async Task<ReturnJeeHR<JeeHRCoCauToChuc>> GetDSCoCauToChuc(string access_token)
         {
-            var traceLog2 = new GeneralLog()
-            {
-                name = "department",
-                data = "",
-                message = "GetDSPhongBan checkusedjeehr GetDSCoCauToChuc"
-            };
-            _logger.LogTrace(JsonConvert.SerializeObject(traceLog2));
-
             string url = $"{_HOST_API_JEEHR}/{GET_COCAUTOCHUC}";
 
             using (var client = new HttpClient())
@@ -87,13 +74,6 @@ namespace JeeAccount.Controllers
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(access_token);
                 var reponse = await client.GetAsync(url);
                 string returnValue = await reponse.Content.ReadAsStringAsync();
-                var traceLog3 = new GeneralLog()
-                {
-                    name = "department",
-                    data = JsonConvert.SerializeObject(returnValue),
-                    message = "GetDSPhongBan checkusedjeehr GetDSCoCauToChuc returnValue"
-                };
-                _logger.LogTrace(JsonConvert.SerializeObject(traceLog3));
                 var res = JsonConvert.DeserializeObject<ReturnJeeHR<JeeHRCoCauToChuc>>(returnValue);
                 return res;
             }
